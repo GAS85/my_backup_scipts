8fea78690b01bad9686e203f148cf4f1803ad95d
Georgiy Sitnikov <GAS85@users.noreply.github.com>
2018-11-16 10:39:19 +0100

    Update systembackup.sh

--- systembackup.sh
+++ systembackup.sh
@@ -8,58 +8,15 @@
 
 nonce=$(md5sum <<< $(ip route get 8.8.8.8 | awk '{print $NF; exit}')$(hostname) | cut -c1-5 )
 
-# PLEASE Create "Backup" Folder in Mega
+# Please do not use root folder
+WORKINGDIR=/home/backup
 
-# Please do not use root folder for backup
-WORKINGDIR=/some/folder
-recipients="your@email.com"
-subject="System backup was done"
-from="noreplay@your.domain"
-megalogin="yyyyyy@TOBE.PROVIDED"
-megapass="xxxxxxTOBEPROVIDED"
+# Other settings
 logfile=/var/log/backup_$nonce.log
+SavePasswordLocal=true
 
-### Please do not edit following Lines:
-LOCKFILE=/tmp/sysbackup
-EMAILFILE=/tmp/sysbackup.mail
-extension=.tar.gpg
-BACKUPNAME=backup-$(date +"%Y-%m-%d")_$nonce$extension
-#Check if Backup file name already taken
-if [ -f "$BACKUPNAME" ]; then
-        # Added time to Backup name
-	echo "WARNING - Backup file $BACKUPNAME exist, will take another name to create backup."
-	BACKUPNAME=backup-$(date +"%Y-%m-%d_%T")_$nonce$extension
-fi
-ToFind="$(echo $BACKUPNAME | cut -c1-6)*$nonce$extension"
-
-#[ -f "$LOCKFILE" ] && exit
-if [ -f "$LOCKFILE" ]; then
-	# Remove lock file if script fails last time and did not run longer than 35 days due to lock file.
-	find "$LOCKFILE" -mtime +35 -type f -delete
-	exit 1
-fi
-
-#Check if Working dir exist
-if [ ! -d "$WORKINGDIR" ]; then
-	echo "Directory $WORKINGDIR does not exist"
-	exit 1
-fi
-
-touch $LOCKFILE
-touch $EMAILFILE
-
-start=`date +%s`
-
-echo "$(date) - INFO - The backup process ($BACKUPNAME) has being started." >> $logfile
-
-# Random password generator. 48 is a password lenght 
-pass="$(gpg --armor --gen-random 1 48)"
-
-cd $WORKINGDIR
-
-# List excluded of folders
+#Folders to be excluded from backup
 excludeFromBackup="--exclude=$WORKINGDIR\
- --exclude=/var/www/nextcloud/data\
  --exclude=/proc\
  --exclude=/tmp\
  --exclude=/mnt\
@@ -72,32 +29,89 @@ excludeFromBackup="--exclude=$WORKINGDIR\
  --exclude=/usr/src/linux-headers*\
  --exclude=/home/*/.gvfs\
  --exclude=/home/*/.cache\
- --exclude=/home/*/.local/share/Trash\"
+ --exclude=/home/*/.local/share/Trash"
+
+. /etc/systembackup.conf
+
+LOCKFILE=/tmp/sysbackup_$nonce
+EMAILFILE=/tmp/sysbackup_$nonce.mail
+extension=.tar.gpg
+BACKUPNAME=backup-$(date +"%Y-%m-%d")_$nonce$extension
+
+if [ -f "$LOCKFILE" ]; then
+	# Remove lock file if script fails last time and did not run longer than 35 days due to lock file.
+	find "$LOCKFILE" -mtime +35 -type f -delete
+	echo "$(date) - WARNING - Another instance blocked backup process."
+	exit 1
+fi
+
+if [ ! -f "/etc/systembackup.conf" ]; then
+	echo "$(date) - ERROR - Config file was not found under /etc/systembackup.conf. Will continuer with default settings."
+else
+	if [ ! -r "/etc/systembackup.conf" ]; then
+		echo "$(date) - ERROR - Config file could not be read."
+		exit 1
+	fi
+fi
+
+# Put output to Logfile and Errors to Lockfile as per https://stackoverflow.com/questions/18460186/writing-outputs-to-log-file-and-console
+exec 3>&1 1>>${logfile} 2>>${LOCKFILE}
+
+#Check if Backup file name already taken
+if [ -f "$BACKUPNAME" ]; then
+        # Added time to Backup name
+	echo "$(date) - WARNING - Backup file $BACKUPNAME exist, will take another name to create backup." | tee /dev/fd/3
+	BACKUPNAME=backup-$(date +"%Y-%m-%d_%T")_$nonce$extension
+fi
+
+#Check if Working dir exist
+if [ ! -d "$WORKINGDIR" ]; then
+	echo "$(date) - Directory $WORKINGDIR does not exist" | tee /dev/fd/3
+	exit 1
+fi
+
+touch $LOCKFILE
+touch $EMAILFILE
+
+start=`date +%s`
+
+echo "$(date) - INFO - The backup process ($BACKUPNAME) has being started."
+
+# Random password generator. 48 is a password lenght 
+pass="$(gpg --armor --gen-random 1 48)"
+
+cd $WORKINGDIR
 
 # Do System backup
-tar -cvp $excludeFromBackup --one-file-system / | gpg --passphrase "$pass" --symmetric --no-tty -o $BACKUPNAME 2 >> $LOCKFILE
+tar -cvp $excludeFromBackup --one-file-system / | gpg --passphrase "$pass" --symmetric --no-tty -o $BACKUPNAME
 
+# Calculating SHA256 of backup file
+sha=$(sha256sum $BACKUPNAME | awk '{printf "%s",$1}' | tr 'a-z' 'A-Z')
+
+# Opt: save password locally if Email fails, or you do not want to send it.
+if [ "$SavePasswordLocal" == true ]; then
+	echo "$(date) - The backup ($BACKUPNAME) was created with password: $pass - SHA256 of backup file: $sha." >> $WORKINGDIR/passes.txt
+fi
 middle=`date +%s`
 
 #Upload backup to Mega
-#megaput -u $megalogin -p $megapass --path /Root/Backup $BACKUPNAME 2>>$LOCKFILE
 upload_command="megaput -u $megalogin -p $megapass --path /Root/Backup $BACKUPNAME"
 
 NEXT_WAIT_TIME=10
 until $upload_command || [ $NEXT_WAIT_TIME -eq 4 ]; do
    sleep $(( NEXT_WAIT_TIME++ ))
-   echo "$(date) - ERROR - Mega Upload was failed, will retry after 10 seconds ($BACKUPNAME)." >> $logfile
+   echo "$(date) - ERROR - Mega Upload was failed, will retry after 10 seconds ($BACKUPNAME)."
 done
 
 #delete local old backups
 # +45 is older than 45 days - basically any other backup.
-find "$ToFind" -mtime +45 -exec rm {} \; 2>>$LOCKFILE
-#find backup*gpg -mtime +45 -exec rm {} \; 2>>$LOCKFILE
+#find "$ToFind" -mtime +45 -exec rm {} \; 2>>$LOCKFILE
+find backup*gpg -mtime +45 -exec rm {} \;
+
+end=`date +%s`
 
 #put collected data into logfile
 [ -s $LOCKFILE ] && cat $LOCKFILE) >> $logfile
-
-end=`date +%s`
 
 #Email Header
 echo "To: $recipients" > $EMAILFILE
@@ -115,14 +129,13 @@ echo "It took `expr $middle - $start`s to create and `expr $end - $middle`s to u
 echo "Have a nice day and check some statistic.<br>">> $EMAILFILE
 echo "<br>">> $EMAILFILE
 echo "Backup size: $(du -h $BACKUPNAME | awk '{printf "%s",$1}').<br>" >> $EMAILFILE
-
-sha=$(sha256sum $BACKUPNAME | awk '{printf "%s",$1}' | tr 'a-z' 'A-Z')
-
 echo "SHA256 of backup file: $sha.<br>" >> $EMAILFILE
 echo "<br>">> $EMAILFILE
 echo "Space information: $(megadf -u $megalogin -p $megapass -h).<br>" >> $EMAILFILE
-[ -s $LOCKFILE ] && echo "Other info: $(cat $LOCKFILE).<br>" >> $EMAILFILE
+[ -s "$LOCKFILE" ] && echo "Other info: $(cat $LOCKFILE).<br>" >> $EMAILFILE
 #echo "<br>">> $EMAILFILE
+
+echo "$(date) - INFO - The backup process ($BACKUPNAME) finished. Backup size: $(du -h $BACKUPNAME | awk '{printf "%s",$1}'). It took `expr $middle - $start`s to create and `expr $end - $middle`s to upload backup file, or `expr $end - $start`s at all."
 
 #send email with password
 cat $EMAILFILE | /usr/sbin/sendmail $recipients
@@ -132,7 +145,5 @@ cat $EMAILFILE | /usr/sbin/sendmail $recipients
 rm $LOCKFILE
 rm $EMAILFILE
 
-# Opt: save password locally if Email fails, or you do not want to send it.
-echo "$(date) - The backup ($BACKUPNAME) was created with password: $pass - SHA256 of backup file: $sha" >> $WORKINGDIR/passes.txt
-
 exit 0
+
